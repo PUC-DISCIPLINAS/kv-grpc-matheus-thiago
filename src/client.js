609b6e9ffa3e96f2fae57222e6215360a2f4e96f
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-03-13 14:52:32 -0300

    feature: client args menu

--- src/client.js
+++ src/client.js
@@ -1,6 +1,45 @@
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-const packageDef = protoLoader.loadSync('kv.proto',{
-
-});
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const packageDef = protoLoader.loadSync("kv.proto", {});
 const kvPackage = grpc.loadPackageDefinition(packageDef).kvPackage;
+
+const commands = {
+  GET: "GET",
+  LIST: "LIST",
+  PUT: "PUT",
+};
+
+const help = () => {
+  console.log("\nPlease, type a valid command\n");
+  console.log(
+    commands.LIST,
+    "\t\t\t List all items\n",
+    commands.GET,
+    " {key} \t\t get value of key\n",
+    commands.PUT,
+    " {key} {value} \t create a new item with key and value"
+  );
+};
+
+const main = () => {
+  const command = process.argv[2];
+  if (!command) {
+    help();
+  } else {
+    switch (command.toUpperCase()) {
+      case commands.GET:
+        console.log("GET");
+        break;
+      case commands.LIST:
+        console.log("LIST");
+        break;
+      case commands.PUT:
+        console.log("PUT");
+        break;
+      default:
+        help();
+    }
+  }
+};
+
+main();
