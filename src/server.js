061a94505725dda713d9f679b7165aa34eb6d350
Matheus <matheusfelipeferreiramartins@gmail.com>
2021-03-13 14:30:41 -0300

    feature: kv.proto

--- src/server.js
+++ src/server.js
@@ -1,1 +1,6 @@
-console.log("Server ready")
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const packageDef = protoLoader.loadSync('kv.proto',{
+
+});
+const kvPackage = grpc.loadPackageDefinition(packageDef).kvPackage;
